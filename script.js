97a3dafec3ecdc49f27d46b50ee87c642913ef29
majkenholst <majhol01@iba.dk>
2025-05-19 09:50:41 +0200

    css og js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const bars = document.querySelector("#bars");
+
+const openNav = () => {
+    document.querySelector("header > nav").classList.toggle("show");
+    document.querySelector("#bars i").classList.toggle("fa-bars");
+    document.querySelector("#bars i").classList.toggle("fa-xmark");
+}
+
+bars.addEventListener("click", openNav);
